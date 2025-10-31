b304a6091f1e4f7af6d34c324ebbe462a2ae6b82
Neto test <kordik.a@yndex.ru>
2025-10-31 18:11:54 +0300

    Changes from 22.10.25

--- FinalCode.py
+++ FinalCode.py
@@ -4,18 +4,25 @@ import requests
 words = input('Enter your word for random cat: ')
 response = requests.get(f'https://cataas.com/cat/says/{words}')
 
+with open(f'Cats/{words}.jpg', 'wb') as f:
+    f.write(response.content)
+
+with open(f'Cats/{words}.json', 'w') as f:
+    data = response.headers
+    keep = ["Content-Length"]
+    keep_dict = {key: data[key] for key in keep}
+    json.dump(keep_dict, f)
 
 """Creating yandex disk file for saving images"""
 
-class YAPI:
 
+class YAPI:
     base_url = 'https://cloud-api.yandex.net'
 
     def __init__(self, token):
         self.header = {'Authorization': f'OAuth {token}'}
 
     def create_folder(self, folder_name):
-
         params = {'path': folder_name}
 
         response = requests.put(f'{self.base_url}/v1/disk/resources',
@@ -23,21 +30,16 @@ class YAPI:
                                 params=params)
         return response.status_code
 
-    def upload_files(self, path_to_file, path_to_disk):
-
-        params = {
-            'path': f'PD-FPY-136/{words}.jpg'
-        }
+    def upload_files(self, local_path, yandex_disk):
+        params = {'path': yandex_disk}
         response = requests.get(f'{self.base_url}/v1/disk/resources/upload', params=params, headers=self.header)
         print(response.status_code)
         upload_link = response.json()['href']
 
-        with open(f'Cats/{words}.jpg', 'rb') as f:
+        with open(local_path, 'rb') as f:
             requests.put(upload_link, files={'file': f})
 
-        params = {
-            'path': f'PD-FPY-136/{words}.json'
-        }
+        params = {'path': f'PD-FPY-136/{words}.json'}
 
         response = requests.get(f'{self.base_url}/v1/disk/resources/upload', params=params, headers=self.header)
         print(response.status_code)
@@ -46,22 +48,9 @@ class YAPI:
         with open(f'Cats/{words}.json', 'r') as f:
             requests.put(upload_link, files={'file': f})
 
-"""Uploading file to yandex disk. After each successful operation shows code 200 
-    as files been uploaded to your account"""
-
-with open (f'Cats/{words}.jpg', 'wb') as f:
-    f.write(response.content)
-
-with open (f'Cats/{words}.json' , 'w') as f:
-    data = response.headers
-    keep = ["Content-Length"]
-    keep_dict = {key: data[key] for key in keep}
-    json.dump(keep_dict, f)
-
-
 
 """Place your token for using this program"""
 
 account = YAPI(token)
 account.create_folder("PD-FPY-136")
-account.upload_files(f'{words}.jpg', f"PD-FPY-136/{words}.jpg")
+account.upload_files(f'Cats/{words}.jpg', f"PD-FPY-136/{words}.jpg")
