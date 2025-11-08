d12845ad7fdaf79c5145969db9a725f0ded2dbad
Neto test <kordik.a@yndex.ru>
2025-11-08 19:52:15 +0300

    Updated_Task

--- FinalCode.py
+++ FinalCode.py
@@ -1,8 +1,13 @@
 import json
+import time
+from tqdm import tqdm
 import requests
 
+
 words = input('Enter your word for random cat: ')
+token = input('Enter your token to upload files to your Yandex Disk: ')
 response = requests.get(f'https://cataas.com/cat/says/{words}')
+
 
 with open(f'Cats/{words}.jpg', 'wb') as f:
     f.write(response.content)
@@ -12,6 +17,7 @@ with open(f'Cats/{words}.json', 'w') as f:
     keep = ["Content-Length"]
     keep_dict = {key: data[key] for key in keep}
     json.dump(keep_dict, f)
+
 
 """Creating yandex disk file for saving images"""
 
@@ -30,27 +36,35 @@ class YAPI:
                                 params=params)
         return response.status_code
 
-    def upload_files(self, local_path, yandex_disk):
+    def upload_files(self, local_path, yandex_disk, local_json, yandex_json):
         params = {'path': yandex_disk}
         response = requests.get(f'{self.base_url}/v1/disk/resources/upload', params=params, headers=self.header)
-        print(response.status_code)
+        # print(response.status_code) --Checking response
         upload_link = response.json()['href']
 
         with open(local_path, 'rb') as f:
             requests.put(upload_link, files={'file': f})
 
-        params = {'path': f'PD-FPY-136/{words}.json'}
+        params = {'path': yandex_json}
 
         response = requests.get(f'{self.base_url}/v1/disk/resources/upload', params=params, headers=self.header)
-        print(response.status_code)
+        # print(response.status_code) --Checking response
         upload_link = response.json()['href']
 
-        with open(f'Cats/{words}.json', 'r') as f:
+        with open(local_json, 'r') as f:
             requests.put(upload_link, files={'file': f})
 
+"""Progress bar by tqdm"""
 
-"""Place your token for using this program"""
+links = ['https://cloud-api.yandex.net/v1/disk/resources', 'https://cloud-api.yandex.net/v1/disk/resources/upload']
+for link in tqdm(links):
+    time.sleep(2)
+
+
+"""For users"""
 
 account = YAPI(token)
 account.create_folder("PD-FPY-136")
-account.upload_files(f'Cats/{words}.jpg', f"PD-FPY-136/{words}.jpg")
+account.upload_files(f'Cats/{words}.jpg', f'PD-FPY-136/{words}.jpg', f'Cats/{words}.json', f'PD-FPY-136/{words}.json' )
+
+
